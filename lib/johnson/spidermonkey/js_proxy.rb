80122a642a21e9799ea8a407814fea5d58fe2b74
John Barnette <jbarnette@gmail.com>
2008-03-31 15:06:46 -0700

    Nodoc for JSProxy.

--- lib/johnson/spidermonkey/js_proxy.rb
+++ lib/johnson/spidermonkey/js_proxy.rb
@@ -1,6 +1,6 @@
 module Johnson #:nodoc:
   module SpiderMonkey #:nodoc:
-    class JSProxy
+    class JSProxy #:nodoc:
       def self.jsend(target, symbol, args)
         if args.last && args.last.is_a?(RubyProxy) && args.last.function?
           block = args.pop
