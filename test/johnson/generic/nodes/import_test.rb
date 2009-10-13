244fb0692053c841dd76ea344a90b9fe2e4700c6
Steven Parkes <smparkes@smparkes.net>
2009-10-13 12:10:16 -0700

    tracemonkey doesn't have import/export, so make those optional

--- test/johnson/generic/nodes/import_test.rb
+++ test/johnson/generic/nodes/import_test.rb
@@ -2,12 +2,14 @@ require File.expand_path(File.join(File.dirname(__FILE__), "helper"))
 
 class ImportTest < Johnson::NodeTestCase
   def test_import
-    assert_sexp([[:import,
-      [
-        [:dot_accessor, [:name, "name1"], [:name, "o"]],
-        [:dot_accessor, [:name, "name2"], [:name, "o"]]
-      ]]], @parser.parse('import o.name1, o.name2;'))
-    assert_ecma('import o.name1, o.name2;',
-      @parser.parse('import o.name1, o.name2;'))
+    begin
+      assert_sexp([[:import,
+        [
+          [:dot_accessor, [:name, "name1"], [:name, "o"]],
+          [:dot_accessor, [:name, "name2"], [:name, "o"]]
+        ]]], @parser.parse('import o.name1, o.name2;'))
+      assert_ecma('import o.name1, o.name2;',
+        @parser.parse('import o.name1, o.name2;'))
+    rescue Johnson::Parser::SyntaxError; end # okay if an interpreter doesn't implement import/export
   end
 end
